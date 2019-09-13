26e1219ba14f60b08970474f2a35ebc733fd453c
Brandon Youngblood <youngbloodbrandon62@gmail.com>
2019-09-13 01:40:31 -0500

    imported external json

--- src/App.js
+++ src/App.js
@@ -3,25 +3,11 @@ import './App.css';
 import Card from './Card/Card';
 import DrawButton from './DrawButton/DrawButton';
 
-//import {DB_CONFIG}  from './Config/Firebase/db_config';
+import http_cards from './Data/http_cards';
 
 function App() {
-  /*
-  const app = firebase.initializeApp(DB_CONFIG);
-  const database = app.database().ref().child('cards');
-  */
-
   //mockdata for init development
-  const [cards, setCards] = useState([
-    { id: 1, question: 'Question 1 goes here', answer: 'Answer 1 goes here' },
-    { id: 2, question: 'Question 2 goes here', answer: 'Answer 2 goes here' },
-    { id: 3, question: 'Question 3 goes here', answer: 'Answer 3 goes here' },
-    { id: 4, question: 'Question 4 goes here', answer: 'Answer 4 goes here' },
-    { id: 5, question: 'Question 5 goes here', answer: 'Answer 5 goes here' },
-    { id: 6, question: 'Question 6 goes here', answer: 'Answer 6 goes here' },
-    { id: 7, question: 'Question 7 goes here', answer: 'Answer 7 goes here' },
-    { id: 8, question: 'Question 8 goes here', answer: 'Answer 8 goes here' },
-  ]);
+  const [cards, setCards] = useState(http_cards.cards);
   const [currentCard, setCurrentCard] = useState({});
   useEffect(() => {
     const currentCards = cards;
