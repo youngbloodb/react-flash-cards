e8dad6e699b9dcf43ab582f80a76eb43386b03c8
youngblood <brandon.youngblood@caemilusa.com>
2020-02-04 20:14:42 -0600

    Added topic filtering and lecture 2 QnA

--- src/App.js
+++ src/App.js
@@ -2,56 +2,68 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Card from './Card/Card';
 import DrawButton from './DrawButton/DrawButton';
-
 import QnA from './Data/QnA';
 
 const App = () => {
-  //states
+  //state getters / setters
   const [cards, setCards] = useState(QnA.cards);
   const [currentCard, setCurrentCard] = useState({});
+  const [currentTopic, setCurrentTopic] = useState(QnA.topics[0]);
 
-  //randomize cards functionality
+  //card selection
   useEffect(() => {
-    const currentCards = cards;
-    setCurrentCard(getRandomCard(currentCards));
-  }, [cards, currentCard]);
+    updateCard();
+  }, [cards, currentTopic]);
 
-  const getRandomCard = (currentCards) => {
+  const getRandomCard = currentCards => {
     const card = currentCards[Math.floor(Math.random() * currentCards.length)];
     return card;
-  }
+  };
 
   const updateCard = () => {
-    const currentCards = cards;
+    const currentCards = getCardsByTopic(currentTopic);
     setCurrentCard(getRandomCard(currentCards));
-  }
+  };
+
+  const getCardsByTopic = topic => {
+    if (topic === 'All topics') {
+      return cards;
+    } else {
+      return cards.filter(card => card.topic === topic);
+    }
+  };
 
   //topics selection
-  const [currentTopic, setCurrentTopic] = useState(QnA.topics[0]);
-  const getTopicOptions = (topics) => {
-    return topics.map((location, index) => <option key={index}>{location}</option>);
-  }
+  const getTopicOptions = topics => {
+    return topics.map((location, index) => (
+      <option key={index}>{location}</option>
+    ));
+  };
 
-  const topicChange = (evt) => {
+  const topicChange = evt => {
     const elem = evt.currentTarget;
     const elemValue = elem.options[elem.selectedIndex].value;
-    console.log('elemValue: ', elemValue);
-    setCurrentTopic(elemValue)
-  }
+    setCurrentTopic(elemValue);
+    updateCard();
+  };
 
   return (
     <div className='App'>
-      <select className="topicsSelect" onChange={evt => topicChange(evt)}>
+      <select className='topicsSelect' onChange={evt => topicChange(evt)}>
         {getTopicOptions(QnA.topics)}
       </select>
       <div className='cardRow'>
-        <Card question={currentCard.question} answer={currentCard.answer} topic={currentCard.topic} />
+        <Card
+          question={currentCard.question}
+          answer={currentCard.answer}
+          topic={currentCard.topic}
+        />
       </div>
       <div className='buttonRow'>
         <DrawButton drawCard={updateCard} />
       </div>
     </div>
   );
-}
+};
 
 export default App;
