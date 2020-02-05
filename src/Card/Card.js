ae81b7906df68720c47bebb1240e395c874bc854
Brandon Youngblood <youngblood@caemilusa.com>
2020-02-05 14:29:16 -0600

    updated card size and json content

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,25 +1,22 @@
 import React from 'react';
 import './Card.css';
+import { Markup } from 'interweave';
 
-const charAtXToUpper = (idx, str) => {
-  return str.charAt(idx).toUpperCase() + str.substring(idx + 1);
-}
-
-const Card = (props) => {
+const Card = props => {
   let { question, answer, topic } = props;
-  if(question !== undefined && answer !== undefined){
-    question = charAtXToUpper(0, question); 
-    answer = charAtXToUpper(0, answer); 
-  }
 
   return (
     <div className='card-container'>
       <div className='card'>
         <div className='front'>
-          <div className='question'>{question}</div>
+          <div className='question'>
+            <Markup content={question} />
+          </div>
         </div>
         <div className='back'>
-          <div className='answer'>{answer}</div>
+          <div className='answer'>
+            <Markup content={answer} />
+          </div>
         </div>
       </div>
       <div className='topic'>{topic}</div>
